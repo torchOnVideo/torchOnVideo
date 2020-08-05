b1a0640b064bdb843a1bcea11fd1b28059364b3e
Akash Manna <akashmanna@gmail.com>
2020-08-05 21:53:27 +0530

    Create action-recognition.py

---
+++ action-recognition.py
@@ -0,0 +1,12 @@
+# Resources for Action Recognition
+
+
+### Scene Debiasing Network for Action Recognition in PyTorch
+Pretrained weights are also available. Also references to a lot of video action datasets
+Repo at: ``` https://github.com/vt-vl-lab/SDN ```
+
+### Temporal Pyramid network [SOTA]
+Repo at: ```https://github.com/decisionforce/TPN```
+
+### TRN-Pytorch
+Repo at: ```https://github.com/zhoubolei/TRN-pytorch```
