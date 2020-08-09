2754e995fa25b64f32992164dddea9e2c7484357
shardulparab97 <shardulsvparab@gmail.com>
2020-08-09 16:23:51 +0530

    Added SRGAN

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import torchOnVideo
 from torchOnVideo.super_resolution.models import DBPNS
 import torchOnVideo.losses
 
-
+base_filter = 256
+feat = 64
+num_stages = 3
+scale_factor = 4
+DBPNS(base_filter, feat, num_stages, scale_factor)
 
 
---
+++ torchOnVideo/super_resolution/iSeeBetter.py
@@ -0,0 +1,7 @@
+
+
+# When the class is called - we can either just use the models with get_models function
+# use train method either with default values or use a json file to change the params
+# use the inference method directly if you have a pretrained model
+class iSeeBetter():
+    pass
