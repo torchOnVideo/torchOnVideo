237e4fce37b15efbc4e2c8b1a27b05cc3efff28b
shardulparab97 <shardulsvparab@gmail.com>
2020-08-09 17:44:52 +0530

    Added iSeeBetter training pipeline

--- torchOnVideo/super_resolution/__init__.py
+++ torchOnVideo/super_resolution/__init__.py
@@ -3,3 +3,9 @@
 # Implement Inheritance
 # Implement different files for train, test and initialize in for each base model
 # Create proper dataloaders and dataset managers
+# Remove deprecated variable API or other deprecated API Calls
+# If needed add a logger too
+# provide utils:
+#       printNetworkArch
+#       loadPreTrainedModel
+#       printCUDAStats
