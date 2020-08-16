7e10ae1bd5722e01cc0a3cc5bcdd6fe4a8085ddb
shardulparab97 <shardulsvparab@gmail.com>
2020-08-16 14:40:33 +0530

    Added changes to ReadMe

--- Video_SuperResolution.md
+++ Video_SuperResolution.md
@@ -8,7 +8,14 @@ https://paperswithcode.com/paper/real-time-single-image-and-video-super
 
 - If needed implement SRCNN
 
+- Deep Video Super-Resolution using HR Optical Flow Estimation (6th Jan 2020)
+        https://arxiv.org/pdf/2001.02129v1.pdf
+        https://github.com/LongguangWang/SOF-VSR Very important
+        Also pull use other models available directly from here
+
 Another option available - no implementation yet
 End-To-End Trainable Video Super-Resolution Based on a New Mechanism for Implicit Motion Estimation and Compensation
 
 
+- Video Quality Metrics: 
+https://github.com/aizvorski/video-quality
--- torchOnVideo/super_resolution/__init__.py
+++ torchOnVideo/super_resolution/__init__.py
@@ -9,3 +9,29 @@
 #       printNetworkArch
 #       loadPreTrainedModel
 #       printCUDAStats
+
+
+# Datasets
+# Label Set
+# Data set
+
+# use pytorch's read video to read from the videofolder dataset
+
+
+# Test all your models and keep
+
+# Metrics needed - Add and test all the required possible metrics for all 4 options
+# Datasets
+# Multi GPU and Data Parallel issues
+# Sphinx Documentation
+
+# Add missing points from py Video Research
+
+
+# Last 5th possible topic
+# Neural Style transfer for videos - https://github.com/gordicaleksa/pytorch-naive-video-nst
+
+
+#Video Dataset Links:
+# https://github.com/MohsenFayyaz89/PyTorch_Video_Dataset/blob/master/GeneralVideoDataset.py
+# https://github.com/Emily0219/video-dataset-preprocess
