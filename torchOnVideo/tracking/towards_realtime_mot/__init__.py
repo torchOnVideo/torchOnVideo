2298fedd4d0466fdb2bc08a493255dc41e7761ec
Akash Manna <Akash1.Manna@ril.com>
2020-08-23 21:50:05 +0530

    following structure for training

---
+++ torchOnVideo/tracking/towards_realtime_mot/__init__.py
@@ -0,0 +1,6 @@
+
+from ..SuperResolution import SuperResolution
+from .SOF_VSR import SOF_VSR
+from .train_model import TrainModel
+from .evaluate_model import EvaluateModel
+from ..models import OFRnet, SRnet, SOFVSR
