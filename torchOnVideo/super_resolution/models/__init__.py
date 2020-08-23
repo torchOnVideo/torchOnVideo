7409eff43df20919896a07a351679b09a0979b56
shardulparab97 <shardulsvparab@gmail.com>
2020-08-23 12:43:44 +0530

    Added all models for super_resolution SOF_VSR

--- torchOnVideo/super_resolution/models/__init__.py
+++ torchOnVideo/super_resolution/models/__init__.py
@@ -1,3 +1,6 @@
 from .dpbns import DBPNS
 from .rbpn import RBPN
 from .SRGAN import SRGAN_Discriminator, SRGAN_Generator
+from .OFRnet import OFRnet
+from .SOFVSR import SOFVSR
+from .SRnet import SRnet
