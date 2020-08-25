8a679c3acffafcf4b4fd57e04bae41c35accdab1
Akash Manna <Akash1.Manna@ril.com>
2020-08-25 14:18:18 +0530

    adding more filex

--- torchOnVideo/tracking/VideoTracking.py
+++ torchOnVideo/tracking/VideoTracking.py
@@ -1,4 +1,18 @@
+"""
+Video Tracking Module
+----------------------------
+This module does Video Tracking
+
+"""
+
+
+
+import torch
+import numpy as np
+
+
 class VideoTracking():
+    
     def __init__(self, tracking_type):
         self.TrackingType = tracking_type
 
--- torchOnVideo/tracking/towards_realtime_mot/__init__.py
+++ torchOnVideo/tracking/towards_realtime_mot/__init__.py
@@ -1,6 +1,4 @@
 
-from ..SuperResolution import SuperResolution
-from .SOF_VSR import SOF_VSR
 from .train_model import TrainModel
 from .evaluate_model import EvaluateModel
 from ..models import OFRnet, SRnet, SOFVSR
