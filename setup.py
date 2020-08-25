bce072b26ff64e364fe2dc09a72ca331271f3a9a
Akash Manna <Akash1.Manna@ril.com>
2020-08-25 20:43:54 +0530

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+        name='torchOnVideo',
+        version='0.1.0',
+        description="A PyTorch Library for Deep Learning on Videos",
+        py_modules=["main"],
+        package_dir={'':'torchOnVideo'},
+)
